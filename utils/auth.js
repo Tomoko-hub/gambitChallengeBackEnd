1f0e2e86bbf165fffd160551f8b7e4dcf74b2683
Tomoko-hub <haisaitomoco@gmail.com>
2023-03-06 20:45:39 +0200

    edit token

--- utils/auth.js
+++ utils/auth.js
@@ -4,12 +4,12 @@ const secret_key = "gambit-challenge"
 const auth = async(req, res, next)=> {
     if (req.method === "GET") {
         return next()
+        //return handler(req,res)
     }
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRhYXZpQGVtYWlsLmNvbSIsImlhdCI6MTY3Nzk1MDE5MywiZXhwIjoxNjc4MDc5NzkzfQ.LOyiWMLP5z3x1EguEgtlbHz5FtUL6xHYJv6CLONVCyI"
-      
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImVtYWlsMSIsImlhdCI6MTY3ODA5OTAzMSwiZXhwIjoxNjc4MjI4NjMxfQ.KG6nGy1LQmaVGZ0uN-5y6qReicRQMrP74I0KmXbRUuo"
     //await req.headers.authorization.split("")[1]
     if (!token){
-        return res.status(400).json({message: "User doesn't have token."})
+        return res.status(401).json({message: "User doesn't have token."})
     }
     try{
         const decoded = jwt.verify(token, secret_key)
